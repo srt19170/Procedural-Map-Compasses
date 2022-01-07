48a6fd25be4fa920c25f728bf20684b141cfbf45
Scott Turner <srt19170@gmail.com>
2022-01-07 18:00:33 -0500

    Initial Part 9 commit

---
+++ cdl.ne
@@ -0,0 +1,55 @@
+# To compile: nearleyc cdl.ne -o cdl.js
+@preprocessor esmodule
+@builtin "number.ne"
+@builtin "string.ne"
+# The first element in the grammar is the default to start
+# parsing.
+# The background consists of one or more background elements
+Compass -> Element:+ {% data => data[0].map(a => a[0]) %}
+WS -> [ \t\n\v\f]:*
+# A list of all the legal CDL elements
+Element -> spaceElement | circleElement | rlineElement | rcircleElement | rtriElement | rpointElement | rtextElement | rarcElement
+# A space is e.g., SPACE(1.5)
+spaceElement -> "SPACE"i WS "(" WS decimal WS ")" WS {% data => ({op: "SPACE", n: data[4]}) %}
+# A circle is e.g., CIRCLE(1, "black", "none")
+circleElement -> "CIRCLE"i WS "(" WS decimal WS "," WS dqstring WS
+                 "," WS dqstring WS ")" WS {% data => ({op: "CIRCLE", lineWidth: data[4], lineColor: data[8], fillColor: data[12]}) %}
+# A radial line element
+rlineElement -> "RLINE"i WS "(" WS decimal WS "," WS decimal WS "," WS decimal WS "," WS decimal WS "," WS dqstring WS ")" WS
+{% data => ({op: "RLINE", start: data[4], repeats: data[8], length: data[12], width: data[16], color: data[20]}) %}
+# A radial circle element
+# RCIRCLE(start, repeats, size, lineWidth, lineColor, fillColor)
+rcircleElement -> "RCIRCLE"i WS "(" WS decimal WS "," WS decimal WS "," WS decimal WS "," WS decimal WS "," WS dqstring WS "," WS dqstring WS ")" WS
+{% data => ({op: "RCIRCLE", start: data[4], repeats: data[8], size: data[12], lwidth: data[16], color: data[20], fill: data[24]}) %}
+# A radial triangle element
+# RTRI(start, repeats, height, width, lineWidth, lineColor, fillColor)
+rtriElement -> "RTRI"i WS "(" WS decimal WS "," WS decimal WS "," WS decimal WS "," WS decimal WS "," WS decimal WS "," WS dqstring WS "," WS dqstring WS ")" WS
+{% data => ({op: "RTRI", start: data[4], repeats: data[8], height: data[12], width: data[16], lwidth: data[20], color: data[24], fill: data[28]}) %}
+# A radial compass point element
+# RPOINT(start, repeats, span, width, lineWidth, lineColor, lightFill, darkFill)
+rpointElement -> "RPOINT"i WS "(" WS decimal WS "," WS decimal WS "," WS decimal WS "," 
+                                  WS decimal WS "," WS decimal WS "," WS dqstring WS "," 
+                                  WS dqstring WS "," WS dqstring WS ")" WS
+                 {% data => ({op: "RPOINT", start: data[4], repeats: data[8], span: data[12],
+                              width: data[16], lwidth: data[20], lcolor: data[24], 
+                              lightFill: data[28], darkFill: data[32]}) %}
+# DQLIST
+# A list of double-quoted strings, possibly empty
+dqList -> "[" WS dqElements WS "]" {% data => data[2] %}
+dqElements -> null {% data => [] %}
+	      | dqstring {% data => data %}
+	      | dqstring WS "," WS dqElements {% data => [data[0], ...data[4]] %}
+# Radial text
+# RTEXT(start, repeats, font, size, color, style, orientation, texts)
+rtextElement -> "RTEXT"i WS "(" WS decimal WS "," WS decimal WS "," WS dqstring WS ","
+	     		        WS decimal WS "," WS dqstring WS "," WS dqstring WS "," 
+				WS dqstring WS "," WS dqstring WS "," WS dqList WS ")" WS
+                 {% data => ({op: "RTEXT", start: data[4], repeats: data[8], font: data[12],
+                              size: data[16], color: data[20], style: data[24], weight: data[28],
+			      orientation: data[32], texts: data[36]}) %}
+# Radial arc
+# RARC(start, repeats, subtend, width, color)
+rarcElement -> "RARC"i WS "(" WS decimal WS "," WS decimal WS "," WS decimal WS ","
+	       	              WS decimal WS "," WS dqstring WS ")" WS
+                 {% data => ({op: "RARC", start: data[4], repeats: data[8], subtend: data[12],
+                              width: data[16], color: data[20]}) %}
---
+++ draw.js
@@ -0,0 +1,197 @@
+//
+//  Implementation of drawing routines for map compasses using SVG & D3
+//
+
+function circle(svg, center, radius, lineWidth, lineColor, fillColor) {
+    svg.append('circle')
+	.attr('cx', center[0])
+	.attr('cy', center[1])
+	.attr('r', radius)
+	.style('stroke-width', lineWidth)
+	.style('stroke', lineColor)
+	.style('fill', fillColor);
+};
+
+function rcircle(svg, center, radius, start, repeats, angle, i, op) {
+    const sx = center[0] + radius * Math.cos(angle);
+    const sy = center[1] - radius * Math.sin(angle);
+    circle(svg, [sx, sy], op.size, op.width, op.color, op.fill);
+};
+
+function line(svg, start, end, width, color) {
+	svg.append('line')
+	    .style('stroke-width', width)
+	    .style('stroke', color)
+	    .attr('x1', start[0])
+	    .attr('y1', start[1])
+	    .attr('x2', end[0])
+	    .attr('y2', end[1]);
+};
+
+function rline(svg, center, radius, start, repeats, angle, i, op) {
+    // Length == 0 indicates a line that goes to the center of the compass
+    if (op.length == 0) op.length = radius;
+    const sx = center[0] + radius * Math.cos(angle);
+    const sy = center[1] - radius * Math.sin(angle);
+    const ex = center[0] + (radius-op.length) * Math.cos(angle);
+    const ey = center[1] - (radius-op.length) * Math.sin(angle);
+    line(svg, [sx, sy], [ex, ey], op.width, op.color);
+};
+
+//
+//  Rotate p2 around p1 by angle (in radians)
+//
+function rotate(p1, p2, angle) {
+    let cos = Math.cos(angle),
+        sin = Math.sin(angle),
+        nx = (cos * (p2[0] - p1[0])) - (sin * (p2[1] - p1[1])) + p1[0],
+        ny = (cos * (p2[1] - p1[1])) + (sin * (p2[0] - p1[0])) + p1[1];
+    return [nx, ny];
+}
+
+const lineFunc = d3.line()
+      .x(pt => pt[0])
+      .y(pt => pt[1])
+      .curve(d3.curveLinearClosed);
+
+function rtri(svg, center, radius, start, repeats, angle, i, op) {    
+    if (op.height == 0) op.height = radius;
+    let triangle = [[center[0]-op.width/2, center[1]-radius], 
+		    [center[0]+op.width/2, center[1]-radius],
+		    [center[0], center[1]-radius+op.height]];
+    triangle = triangle.map(pt => rotate(center, pt, angle));
+    svg.append('path')
+	.style('stroke-width', op.lwidth)
+	.style('stroke', op.color)
+	.style('fill', op.fill)
+	.attr('d', lineFunc(triangle));
+};
+
+function rpoint(svg, center, radius, startAngle, repeats, angle, i, op) {
+    let start = [center[0], center[1]-radius];
+    let end = center;
+    // Midpoint is the point on the center line where the
+    // diamond will have its maximum width
+    let midpoint;
+    if (op.span <= 1) {
+	// Treat span as a % of radius
+	op.span = op.span*radius;
+    };
+    midpoint = [center[0], center[1]-radius+op.span];
+    // The maximum width is one that just touches the adjacent point.
+    // Phi is the angle between adjacent points.
+    const phi = (2*Math.PI)/repeats;
+    const maxWidth = Math.tan(phi/2)*(radius-op.span)*2;
+    // Calculate the actual width based on width
+    if (op.width <= 1) {
+	op.width = op.width*maxWidth;
+    };
+    // Calculate the two side points
+    let side1 = [center[0]-op.width/2, center[1]-radius+op.span];
+    let side2 = [center[0]+op.width/2, center[1]-radius+op.span];
+    // Rotate the diamond points.
+    start = rotate(center, start, angle);
+    end = rotate(center, end, angle);
+    side1 = rotate(center, side1, angle);
+    side2 = rotate(center, side2, angle);
+    // Create and fill the left (light) side area
+    const left = [start, side1, end];
+    svg.append('path')
+	.style('stroke-width', 0)
+	.style('stroke', 'none')
+	.style('fill', op.lightFill)
+	.attr('d', lineFunc(left));
+    // Create and fill the right (dark) side area
+    const right = [start, side2, end];
+    svg.append('path')
+	.style('stroke-width', 0)
+	.style('stroke', 'none')
+	.style('fill', op.darkFill)
+	.attr('d', lineFunc(right));
+    // Draw the outline
+    let p = [start, side1, end, side2];
+    svg.append('path')
+	.style('stroke-width', op.lwidth)
+	.style('stroke', op.lcolor)
+	.style('fill', 'none')
+	.attr('d', lineFunc(p));
+};
+
+function rad2degrees(rad) {
+    return rad * (180/Math.PI);
+};
+
+function rtext(svg, center, radius, startAngle, repeats, angle, iteration, op) {
+    let x = center[0];
+    let y = center[1]-radius;
+    // If orientation == vertical, then rotate [x, y] around the center by
+    // angle to find the spot for the label.
+    if (op.orientation == 'vertical') {
+	[x, y] = rotate(center, [x, y], angle-Math.PI/2);
+    };
+    // Now draw the label
+    let label = svg.append('text')
+	.attr('x', x)
+	.attr('y', y)
+	.style('font-family', op.font)
+	.style('fill', op.color)
+	.style('font-size', op.size)
+	.style('font-style', op.style)
+	.style('font-weight', op.weight)
+	.style('text-anchor', 'middle')
+	.text(op.texts[iteration]);
+    // If orientation == radial, then we've drawn the label at 12 o'clock
+    // and need to use SVG transform to rotate it around to the correct
+    // spot.
+    if (op.orientation != 'vertical') {
+	label.attr('transform', 'rotate('+rad2degrees(angle-Math.PI/2)+','+center[0]+','+center[1]+')');
+    } else {
+	label.style('dominant-baseline', 'central');
+    };
+};
+
+// Make a circular arc
+function makeCircularArc(center, radius, start, end, num) {
+    const [x, y] = center;
+    const result = [];
+    const step = (end-start)/num;
+    for(let t=start;t<end;t += step) {
+	result.push([radius*Math.cos(t)+x,radius*Math.sin(t)+y]);
+    };
+    result.push([radius*Math.cos(end)+x,radius*Math.sin(end)+y]);
+    return result;
+};
+
+function rarc(svg, center, radius, startAngle, repeats, angle, iteration, op) {
+    const arcStart = angle-0.5*op.subtend;
+    const arcEnd = angle+0.5*op.subtend;
+    const outsideEdge = makeCircularArc(center, radius, arcStart, arcEnd, 20);
+    const insideEdge = makeCircularArc(center, radius-op.width, arcStart, arcEnd, 20);
+    const polygon = outsideEdge.concat(insideEdge.reverse());
+    svg.append('path')
+	.style('stroke-width', 0)
+	.style('stroke', 'none')
+	.style('fill', op.color)
+	.attr('d', lineFunc(polygon));
+};
+
+function repeat(svg, center, radius, start, repeats, op, fn) {
+    let angle = start+(Math.PI/2);
+    for(let i=0;i<repeats;i++) {
+	fn(svg, center, radius, start, repeats, angle, i, op);
+	angle += (Math.PI*2)/repeats;
+    };
+};
+
+export default {
+    circle: circle,
+    rcircle: rcircle,
+    rline: rline,
+    repeat: repeat,
+    rtri: rtri,
+    rpoint: rpoint,
+    rtext: rtext,
+    rarc: rarc,
+
+    dummy: null
+};
